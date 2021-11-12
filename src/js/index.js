59e8b3e5bfb5ba399cd8709f3da687952271bda0
Alona-Bulatkina <83859343+Alona-Bulatkina@users.noreply.github.com>
2021-11-12 08:57:30 +0200

    add index.html

--- src/js/index.js
+++ src/js/index.js
@@ -1,62 +1,58 @@
-import './css/styles.css';
+import '../css/styles.css';
 import debounce from 'lodash.debounce';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import { fetchCountries } from './js/services/fetchCountries';
+import { fetchCountries } from './services/fetchCountries';
+import countriesMarkupTml from '../templates/countriesMarkup.hbs';
+import oneCountriesMarkupTml from '../templates/oneCountryMarkup.hbs';
 
 const DEBOUNCE_DELAY = 300;
 
-const searchBox = document.querySelector('#search-box');
-const countryList = document.querySelector('.country-list');
-const countryInfo = document.querySelector('country-info');
 
-searchBox.addEventListener('input', debounce(() => {
-    if (searchBox.value.trim() === '') {
-        countryList.innerHTML = '';
-        countryInfo.innerHTML = '';
+const searchbox = document.querySelector('#search-box');
+const countryListEl = document.querySelector('.country-list');
+const countryInfoEl = document.querySelector('.country-info');
+
+let markup = '';
+
+searchbox.addEventListener('input', debounce(() => {
+    if (searchbox.value.trim() === '') {
+        countryListEl.innerHTML = '';
+        countryInfoEl.innerHTML = '';
         return;
     };
-    fetchCountries(searchBox.value.trim())
+    fetchCountries(searchbox.value.trim())
       .then(userdata => showCountries(userdata))
       .catch(error => showError(error));
   }, DEBOUNCE_DELAY));
 
-  function showCountries(counrtries) {
-    countryList.innerHTML = '';
-    countryInfo.innerHTML = '';
+
+function showCountries(counrtries) {
+    countryListEl.innerHTML = '';
+    countryInfoEl.innerHTML = '';
 
      if (counrtries.length > 10) {
      return Notify.success("Too many matches found. Please enter a more specific name.");
     }
 
     if ((counrtries.length >= 2) && (counrtries.length <= 10)) {
-        countriesMarkup(counrtries);
-        return countryList.insertAdjacentHTML('beforeend', markup);
+        markup = countriesMarkup(counrtries);
+        return countryListEl.insertAdjacentHTML('beforeend', markup);
     }
 
-    oneCountryMarkup(...counrtries);
-    return countryInfo.insertAdjacentHTML('beforeend', markup);
+    markup = oneCountryMarkup(counrtries);
+    return countryInfoEl.insertAdjacentHTML('beforeend', markup);
 }
 
-function countriesMarkup (counrtries) {
-    markup = counrtries.map(item => 
-            `<li>
-            <img src="${item.flags.svg}" class="avatar" alt="flag" width="30" />
-            <span>${item.name.official}</span></li>`
-        ).join('');
+function countriesMarkup(counrtries) {
+    return countriesMarkupTml(counrtries);
 }
 
-function oneCountryMarkup ({flags, name,  capital, population, languages}) {
-    markup = `<img src="${flags.svg}" class="avatar" alt="flag" width="30" />
-            <span class = 'title'>${name.official}</span>
-            <ul class = 'text'>
-            <li>Capital: <span class = 'text__normal'>${capital}</span></li>
-            <li>Population: <span class = 'text__normal'>${population}</span></li>
-            <li>Languages: <span class = 'text__normal'>${Object.keys(languages).join(', ')}</span></li>
-            </ul>`;
+function oneCountryMarkup(counrtries) {
+    return oneCountriesMarkupTml(counrtries);
 }
 
 function showError() {
-    countryList.innerHTML = '';
-    countryInfo.innerHTML = '';
+    countryListEl.innerHTML = '';
+    countryInfoEl.innerHTML = '';
   return Notify.failure("Oops, there is no country with that name");
 }
